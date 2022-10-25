638612d7ac412fab2b44001c36b050d8ded49ac8
Drew <webmaster@dhornbein.com>
2022-10-25 03:09:06 -0600

    a first stroke

---
+++ app.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="flex flex-col items-center">
+    <title>Ritual Point Art & Divination</title>
+    <section>
+      <img src="~/assets/img/logo_mark.png" alt="" class="w-[50vw] mx-auto mb-10 max-w-[50vh]">
+      <h1><img src="~/assets/img/logo.png" alt="Ritual Point"></h1>
+      <div>
+        <h2 class="text"><img src="~/assets/img/txt_art.png" alt="Art"> <img src="~/assets/img/txt_plus.png" alt="+"> <img src="~/assets/img/txt_divination.png" alt="Divinations"></h2>
+      </div>
+    </section>
+    <img src="~/assets/img/squiggle.png" alt="" class="max-w-[150px]">
+    <section>
+      <h2 class="title mb-4">I make art for the</h2>
+      <img src="~/assets/img/skin.png" alt="Skin">
+      <h2 class="title mb-4">& read</h2>
+      <img src="~/assets/img/tarot.png" alt="Tarot" class="max-w-[200px]">
+    </section>
+    <img src="~/assets/img/lines.png" alt="" class="max-w-[150px]">
+    <section>
+      <div class="offerings intuitive">
+        <h3 class="title"><img src="~/assets/img/txt_intuitive.png" alt="Intuitive"></h3>
+        <p>Move with me through a process to intuit the artwork.</p>
+        <BaseButton href="//cal.com/ritualpoint/consult">Schedule</BaseButton>
+        <p class="font-weird">You want something but don't yet know what it is.</p>
+      </div>
+      <div class="offerings flash">
+        <h3 class="title"><img src="~/assets/img/txt_flash.png" alt="Flash"></h3>
+        <p>Choose art from my magic book</p>
+        <BaseButton href="//instagram.com/ritualpoint">@ritualPoint</BaseButton>
+        <p class="font-weird">You'll know when you see it...</p>
+      </div>
+      <div class="offerings original">
+        <h3 class="title"><img src="~/assets/img/txt_original.png" alt="Original"></h3>
+        <p>Co-Create original artwork</p>
+        <BaseButton href="//cal.com/ritualpoint/consult">Schedule</BaseButton>
+        <p class="font-weird">You know what you want and need help giving it form.</p>
+      </div>
+    </section>
+    <img src="~/assets/img/eye.png" alt="" class="max-w-[150px] my-20">
+    <section>
+      <div class="offerings divination">
+        <h3 class="title"><img src="~/assets/img/txt_divination.png" alt="Divination"></h3>
+        <p>Sit for a reading</p>
+        <BaseButton href="//cal.com/ritualpoint/reading-60">Get Started</BaseButton>
+        <p class="font-weird">Unstick and un-earth wisdom.</p>
+      </div>
+    </section>
+    <section>
+      <h2 class="title font-weird mb-4">Who Am I?</h2>
+      <p>I notice the moon's phases. I draw from the well of inspiration. I'm just some guy with ink stained hands. I pray to the trickster god. I laugh myself into cracking screams. I believe in</p>
+      <img src="~/assets/img/txt_art.png" alt="Art">
+      <p>Question what you think you need to know about me and wonder why you need to know it.</p>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+@import url("https://use.typekit.net/jsv6drp.css");
+body {
+  @apply bg-stone-900 text-white p-4 max-w-screen-md mx-auto;
+  font-family: nitti-typewriter-normal, monospace;
+}
+.font-weird {
+  font-family: nitti-typewriter-open, monospace;
+}
+.text {
+  @apply flex flex-wrap flex-grow-0 gap-2 justify-center;
+  img {
+    @apply max-h-8;
+  }
+}
+section {
+  @apply flex flex-col items-center justify-center min-h-[90vh] py-20;
+}
+
+h2.title {
+  @apply text-2xl;
+}
+
+.offerings {
+  @apply flex flex-col gap-4 my-10;
+}
+</style>
---
+++ components/base/button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="navigate">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps(['href'])
+console.log(props.href);
+
+function navigate() {
+  if (props.href.startsWith('//')) {
+    window.open(props.href, '_blank')
+  } else {
+    navigateTo(props.href)
+  }
+}
+</script>
+
+<style lang="scss">
+button {
+  @apply bg-stone-900 text-white p-4 pt-2 text-2xl w-60 h-40 leading-none;
+  font-family: nitti-typewriter-open, monospace;
+  background: url(~/assets/img/button.png) no-repeat center;
+  background-size: 15rem auto;
+  &:hover {
+    background-image: url(~/assets/img/button_hover.png);
+  }
+  &:active {
+    @apply pt-6;
+    background-image: url(~/assets/img/button_click.png);
+  }
+}
+</style>
