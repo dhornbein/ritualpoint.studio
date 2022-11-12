14ddcff7a49c210adebadb6a5c8f6f3db9763a8b
Drew <webmaster@dhornbein.com>
2022-11-12 11:55:42 -0700

    fixed the og

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,9 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'Ritual Point Studio: Art & Divination from the mind of Drew H' },
         { hid: 'og-type', property: 'og:type', content: 'website' },
-        { hid: 'og-title', property: 'og:title', content: 'My Title' },
-        { hid: 'og-desc', property: 'og:description', content: 'This is a sweet post' },
-        { hid: 'og-image', property: 'og:image', content: 'https://ritualpoint.studio/og-default.jpg' },
+        { hid: 'og-title', property: 'og:title', content: 'Ritual Point Art & Divination' },
+        { hid: 'og-desc', property: 'og:description', content: 'Tattoo art and tarot divination' },
+        { hid: 'og-image', property: 'og:image', content: 'https://ritualpoint.studio/img/og-default.jpg' },
         { hid: 'og-url', property: 'og:url', content: 'https://ritualpoint.studio/' },
         { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
       ],
