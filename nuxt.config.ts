4e17bdc9e9d63316296e02fa95459765c735a11b
Drew <webmaster@dhornbein.com>
2024-01-26 20:27:39 -0700

    adds gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-gtag'],
   css: ['~/assets/css/main.scss'],
   app: {
     head: {
